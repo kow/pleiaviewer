23ff05ea20208fd2e10f9cbdda5af511e92ac031
Patrick Sapinski <patrick@sapinski.com>
2010-10-24 04:51:20 -0400

    fixed skins

--- linden/indra/newview/viewer_manifest.py
+++ linden/indra/newview/viewer_manifest.py
@@ -105,7 +105,7 @@ class ViewerManifest(LLManifest):
 
         # skins
         if self.prefix(src="skins"):
-                self.path("paths.xml")
+                self.path("*.xml")
                 # include the entire textures directory recursively
                 if self.prefix(src="*/textures"):
                         self.path("*.tga")
